f7135b35b7b4ae3d93607eb6653e0dcbde48e78c
thibault.bourgeois <thibault.bourgeois@thalesgroup.com>
2025-02-26 15:14:45 +0100

    Initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# entrypoint.sh
+
+cd /app
+
+# Exécute la commande gunicorn pour démarrer l'application
+exec gunicorn -w 4 -b 0.0.0.0:5000 app:app
---
+++ src/app.py
@@ -0,0 +1,31 @@
+from myfunctions import *
+from flask import Flask, request, jsonify
+
+ENDPOINT = "http://192.168.0.1"
+
+app = Flask(__name__)
+
+@app.route('/send', methods=['POST'])
+def send_message():
+    
+    recipient = request.form.get("recipient")
+    message = request.form.get("message")
+    
+    if not recipient:
+        return jsonify({"response": "Error : Recipient missing"}), 400
+    
+    if not message:
+        return jsonify({"response": "Error : Message missing"}), 400
+    
+    try:
+        is_site_accessible()
+        login()
+        send_sms(recipient, message)
+        logout()
+        return jsonify({"response": "success"})
+    except Exception as e:
+        return jsonify({"response": f"Error : {e}"}), 400
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
